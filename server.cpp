37927ccbe69f3d9430d77dac9f14138c092e06cf
Yousif Al-Khalidi <al.yousif206@gmail.com>
2026-02-15 21:56:23 +0000

    switched to threads for persistence and initialised dict

--- server.cpp
+++ server.cpp
@@ -1,20 +1,75 @@
-#include <stdint.h>
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
-#include <errno.h>
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <cstdlib>
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <netinet/ip.h>
-#include <iostream>
-#include <cerrno>
-#include <cstring>
-#include <signal.h>
+#include <thread>
+#include <strings.h>
+#include <map>
+#include <mutex>
+
+std::map<std::string, std::string> g_data;
+std::mutex g_data_mutex;
+
+void handle_client(int connfd) {
+    char buffer[1024] = {0};
+
+    while(true){
+        memset(buffer, 0, sizeof(buffer));
+
+        ssize_t bytes_received = read(connfd, buffer, sizeof(buffer)-1);
+        if(bytes_received <= 0){
+            break;
+        }
+
+        if(strncasecmp(buffer, "*2", 2) == 0 && strcasestr(buffer, "COMMAND")) {
+            const char* reply = "+OK\r\n";
+            write(connfd, reply, strlen(reply));
+            continue;
+        }
+
+        if(buffer[0] == '*'){
+            char* p = buffer;
+
+            char* dollarsign = strchr(p, '$');
+            if(dollarsign) {
+                char* stringstart = strchr(dollarsign, '\n') + 1;
+                if(stringstart) {
+                    if(strncmp(stringstart, "ECHO", 4) == 0){
+                        char* nextdollar = strchr(stringstart, '$');
+                        if(nextdollar){
+                            int lenmessage = atoi(nextdollar+1);
+                            char* messagestart = strchr(nextdollar, '\n') + 1;
+                            if(messagestart){
+                                std::string reply = "$" + std::to_string(lenmessage) + "\r\n";
+                                reply.append(messagestart, lenmessage);
+                                reply += "\r\n";
+                                
+                                write(connfd, reply.c_str(), reply.length());
+                                continue;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        if(strstr(buffer, "PING")){
+            const char* reply = "+PONG\r\n";
+            write(connfd, reply, strlen(reply));
+        }
+        else{
+            const char* err = "-unknown command\r\n";
+            write(connfd, err, strlen(err));
+        }
+    }
+    close(connfd);
+}
 
 int main(){
-    signal(SIGCHLD, SIG_IGN);
-
     int val = 1;
     int fd = socket(AF_INET, SOCK_STREAM, 0);
     setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &val, sizeof(val));
@@ -36,53 +91,7 @@ int main(){
             continue;
         }
 
-        int pid = fork();
-        if (pid == 0){
-            close(fd);
-            char buffer[64] {};
-            while(1){
-                ssize_t bytes_received = read(connfd, buffer, sizeof(buffer)-1);
-                if(bytes_received <= 0){
-                    break;
-                }
-                buffer[bytes_received] = '\0';
-                std::cout << buffer << '\n';
-
-                if(buffer[0] == '*'){
-                    char* p = buffer;
-
-                    char* dollarsign = strchr(p, '$');
-                    char* stringstart = strchr(dollarsign, '\n') + 1;
-
-                    if(strncmp(stringstart, "ECHO", 4) == 0){
-                        char* nextdollar = strchr(stringstart, '$');
-                        if(nextdollar){
-                            int lenmessage = atoi(nextdollar+1);
-                            char* messagestart = strchr(nextdollar, '\n') + 1;
-
-                            std::string reply = "$" + std::to_string(lenmessage) + "\r\n";
-                            reply.append(messagestart, lenmessage);
-                            reply += "\r\n";
-                            
-                            write(connfd, reply.c_str(), reply.length());
-                            continue;
-                        }
-                    }
-                }
-
-                if(strstr(buffer, "PING")){
-                    const char* reply = "+PONG\r\n";
-                    write(connfd, reply, strlen(reply));
-                }
-                else{
-                    const char* err = "-ERR unknown command\r\n";
-                    write(connfd, err, strlen(err));
-                }
-            }
-            close(connfd);
-            exit(0);
-        }
-        close(connfd);
+        std::thread(handle_client, connfd).detach();
     }
 
     return 0;
